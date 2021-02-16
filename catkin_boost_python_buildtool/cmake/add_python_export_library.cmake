eb73566f97a667b21e7d686fadda8d018363aac8
Yannick Huber <huberya@student.ethz.ch>
2021-02-16 16:03:55 +0100

    check major

--- catkin_boost_python_buildtool/cmake/add_python_export_library.cmake
+++ catkin_boost_python_buildtool/cmake/add_python_export_library.cmake
@@ -19,8 +19,6 @@ FUNCTION(add_python_export_library TARGET_NAME PYTHON_MODULE_DIRECTORY)
   if (DEFINED MY_ARGS_VERSION)
     set(TARGET_VERSION ${MY_ARGS_VERSION})
   else()
-    #Ros Noetic & ubuntu 20
-    message(${PYTHON_VERSION_MAJOR})
     if(${PYTHON_VERSION_MAJOR} EQUAL 2)
       set(TARGET_VERSION 2.7)
     else()
